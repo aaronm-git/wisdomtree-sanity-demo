36a53579b9711b413e1633f6b9e24137b4290914
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2025-07-14 22:43:38 -0400

    remove comment

--- app/components/content/whats-new-block/index.tsx
+++ app/components/content/whats-new-block/index.tsx
@@ -5,7 +5,6 @@ import RecentPosts from './recent-posts';
 
 async function WhatsNewBlock({ block, language }: { block: WhatsNewBlockType; language: string }) {
   const posts = await client.fetch(GET_POSTS_BY_LANGUAGE, { language });
-  console.log(posts, '@ server level');
   return (
     <section className="bg-gray-200 px-4 py-10">
       <div className="container mx-auto">
